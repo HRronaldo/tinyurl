0e7184c98458125eb0f6911644d5c2d04fb37a55
jiangpancjy <13738986808@163.com>
2020-09-20 10:35:35 +0800

    create model Long2Short

--- conversion/models.py
+++ conversion/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Long2Short(models.Model):
+    long_url = models.CharField(max_length=2000)
+    short_key = models.CharField(max_length=50)
+    create_date = models.DateTimeField("date create")
+    modified_data = models.DateTimeField("date modified", null=True)
+    visit_sum = models.IntegerField(default=0)
+    ip_address = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        content = """
+            long_url: {} ==> short_url: https://192.168.33.10:8080/tinyurl/{}
+        """.format(self.long_url, self.short_key)
+
+        return content
